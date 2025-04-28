7af1c6c4730079f8259ca9da33ba57dcfea99b1d
pswbuild <pswbuild@oppo.com>
2025-04-28 20:39:21 +0800

    Synchronize code for OPPO CPH2721_15.0.0.606(EX01) Based on QCOM release TAG: kernel/build:AU_LINUX_KERNEL.PLATFORM.4.0.R1.00.00.00.061.068 platform/vendor/nxp/opensource/driver:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/opensource/camera-kernel:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/opensource/dataipa:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/opensource/display-drivers:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/opensource/mm-drivers:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/opensource/mmrm-driver:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/opensource/synx-kernel:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/opensource/video-driver:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/proprietary/mm-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom-opensource/bt-devicetree:AU_TECHPACK_BTFM.LA.1.0.R3.00.00.00.000.010 platform/vendor/qcom-opensource/bt-kernel:AU_TECHPACK_BTFM.LA.1.0.R3.00.00.00.000.010 platform/vendor/qcom-opensource/data-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom-opensource/data-kernel:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom-opensource/devicetree:AU_LINUX_KERNEL.PLATFORM.4.0.R1.00.00.00.061.060 platform/vendor/qcom-opensource/eSE-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom-opensource/nfc-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom-opensource/synx-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom-opensource/wlan/fw-api:AU_TECHPACK_WLAN.LA.1.1.R1.00.00.00.000.009 platform/vendor/qcom-opensource/wlan/platform:AU_TECHPACK_WLAN.LA.1.1.R1.00.00.00.000.009 platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn:AU_TECHPACK_WLAN.LA.1.1.R1.00.00.00.000.009 platform/vendor/qcom-opensource/wlan/qcacld-3.0:AU_TECHPACK_WLAN.LA.1.1.R1.00.00.00.000.009 platform/vendor/qcom-opensource/wlan/utils/sigma-dut:AU_TECHPACK_WLAN.LA.1.1.R1.00.00.00.000.009 platform/vendor/qcom-opensource/wlan/wlan-devicetree:AU_TECHPACK_WLAN.LA.1.1.R1.00.00.00.000.009 platform/vendor/qcom-proprietary/audio-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.029 platform/vendor/qcom-proprietary/camera-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom-proprietary/display-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom-proprietary/graphics-devicetree:AU_TECHPACK_GRAPHICS.LA.15.1.R1.00.00.00.000.012 platform/vendor/qcom-proprietary/mmrm-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom-proprietary/video-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom/opensource/audio-kernel-ar:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom/opensource/datarmnet:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom/opensource/datarmnet-ext:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom/opensource/dsp-devicetree:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom/opensource/dsp-kernel:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom/opensource/graphics-kernel:AU_TECHPACK_GRAPHICS.LA.15.1.R1.00.00.00.000.012 platform/vendor/qcom/opensource/securemsm-kernel:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/qcom/opensource/spu-kernel:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/st/opensource/driver:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042 platform/vendor/st/opensource/eSE-driver:AU_LINUX_ANDROID_LA.VENDOR.15.4.1.R1.11.00.00.1214.042

---
+++ vendor/qcom/opensource/display-drivers/config/gki_parrotdispconf.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+//#ifdef OPLUS_FEATURE_DISPLAY
+#define OPLUS_FEATURE_DISPLAY 1
+#define OPLUS_FEATURE_DISPLAY_ADFR 1
+#define OPLUS_FEATURE_DISPLAY_HIGH_PRECISION 1
+#define OPLUS_FEATURE_DISPLAY_TEMP_COMPENSATION 1
+#define OPLUS_FEATURE_DISPLAY_ONSCREENFINGERPRINT 1
+#define OPLUS_TRACKPOINT_REPORT 1
+//#endif /* OPLUS_FEATURE_DISPLAY */
+#define CONFIG_DRM_MSM 1
+#define CONFIG_DRM_MSM_SDE 1
+#define CONFIG_SYNC_FILE 1
+#define CONFIG_DRM_MSM_DSI 1
+#define CONFIG_DSI_PARSER 1
+#define CONFIG_DRM_SDE_WB 1
+#define CONFIG_DRM_MSM_REGISTER_LOGGING 1
+#define CONFIG_DRM_SDE_EVTLOG_DEBUG 1
+#define CONFIG_QCOM_MDSS_PLL 1
+#define CONFIG_GKI_DISPLAY 1
